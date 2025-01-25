3b301d805df7ad86afb61c115cfa6306354686f1
cheqii <qiqitaro@gmail.com>
2025-01-26 03:55:48 +0700

    explore progress (uncomplete)

---
+++ Assets/Qiqi/Script/ExploreProgression.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExploreProgression : MonoBehaviour
+{
+    [Header("Explore Progression Slider")]
+    [SerializeField] private Slider progressSlider;
+
+    [Header("Player Movement")]
+    [SerializeField] private MovementController playerMovement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
